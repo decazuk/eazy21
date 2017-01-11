31b46370d7d2aa17adaf498fd39a9cf106301eb7
zuoyl <zuoyl@creatingev.com>
2017-01-11 22:14:25 +0800

    add qlearner

--- main.py
+++ main.py
@@ -1,46 +1,8 @@
 from easy21env import *
-import numpy as np
+from QLearner import *
 import matplotlib as mpl
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
-
-class QLearner(object):
-    def __init__(self, num_states, num_actions):
-        self.num_states = num_states
-        self.num_actions = num_actions
-        self.q_table = {}
-        self.n_table = {}
-
-    def reset_tables(self):
-        for s in range(self.num_states):
-            self.q_table[s] = np.zeros(self.num_actions)
-            self.n_table[s] = 0
-
-    def choose_action(self, state, action_space):
-        if self.choose_random_action(state):
-            return action_space[random.randint(0, self.num_actions - 1)]
-        else:
-            return self.choose_greedy_action(state, action_space)
-
-    def choose_greedy_action(self, state, action_space):
-        return action_space[self.q_table[state].argsort()[-1]]
-
-    def choose_random_action(self, state):
-        N0 = 100
-        random_rate = float(N0) / (N0 + self.n_table[state])
-        return (1 - random_rate) <= np.random.uniform(0, 1)
-
-    def update_table(self, state, action, reward):
-        original_count = self.n_table[state]
-        self.n_table[state] = original_count + 1
-        original_reward = self.q_table[state][action]
-        self.q_table[state][action] = (original_reward * original_count + reward) / (original_count + 1)
-
-    def max_win_rate(self, state):
-        return np.amax(self.q_table[state])
-
-    def win_rate(self, state, action):
-        return self.q_table[state][action]
 
 def easy21_with_monte_carlo():
     env = Easy21Env()
