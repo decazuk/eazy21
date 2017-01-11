64dc51bb2d3f8941bad4a4ae6cbec8025f532b1d
zuoyl <zuoyl@creatingev.com>
2017-01-11 22:48:49 +0800

    remove unused code

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from easy21env import *
-from qlearner import *
 import matplotlib as mpl
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
+from easy21env import *
 from monte_carlo_learner import * 
 from sarsa_lambda_learner import * 
 
@@ -21,44 +20,6 @@ def draw_dragram(learner, env):
     ax.scatter(x, y, z, label='win rate dispatch')
     ax.legend()
     plt.show()
-
-def easy21_with_sara_lambda():
-    env = Easy21Env()
-    learner = QLearner(num_states = env.number_states, num_actions = env.number_actions)
-    learner.reset_tables()
-
-    def play_and_train_game(lamda):
-        e_table = {}
-        for s in range(env.number_states):
-            e_table[s] = np.zeros(env.number_actions)
-        gamma = 1.0
-        state, terminal, reward = env.init_game()
-        action = learner.choose_action(state, env.action_space)
-        while(not terminal):
-            state_prime, terminal, reward = env.step(state, action)
-            learner.n_table[state] += 1
-            delta = 0.0
-            if terminal:
-                delta = reward - learner.win_rate(state, action)
-            else:
-                action_prime = learner.choose_action(state_prime, env.action_space)
-                delta = reward + gamma * learner.win_rate(state_prime, action_prime) - learner.win_rate(state, action)
-            e_table[state][action] = e_table[state][action] + 1
-            for s in range(env.number_states):
-                for a in range(env.number_actions):
-                    alpha = 1.0 / learner.n_table[s] if learner.n_table[s] != 0 else 0
-                    learner.q_table[s][a] += alpha * delta * e_table[s][a]
-                    e_table[s][a] = gamma * lamda * e_table[s][a]
-            if not terminal:
-                state = state_prime
-                action = action_prime
-    i = 0
-    while(True):
-        if (i > 0 and i % 1000 == 0):
-            draw_dragram(learner, env)
-        play_and_train_game(0.1)
-        i = i + 1
-
 
 if __name__ == "__main__":
     env = Easy21Env()
